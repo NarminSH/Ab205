fa24052b798c58da1f51d9d904efb156814f0b86
NarminSH <nshivakhanova@gmail.com>
2024-11-20 16:32:17 +0400

    front to back

---
+++ ZonApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ZonApp.Models;
+
+namespace ZonApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           List<User> users = new List<User> 
+           { 
+               new User() { Id = 1, Name = "Fizi" },
+               new User() { Id = 2, Name = "Togrul" } 
+           };
+            
+            //ViewBag.users = users;
+            ViewData["users"] ="null";
+            //TempData["message"] = "hello from temp data";
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
